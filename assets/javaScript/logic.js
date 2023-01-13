5a79a16832ad71b719b16e694c7b71f6dc4eeed4
FrantiskaAli <tiskarechk@gmail.com>
2023-01-13 20:12:32 +0000

    This is my progress in the office hours, displayquestion function is now working, i have yet to make the answer buttons

--- assets/javaScript/logic.js
+++ assets/javaScript/logic.js
@@ -1,6 +1,10 @@
 //define all variables that will be used in global scope.
 const timerDisplay = document.getElementById('time');
-
+const start = document.getElementById('start');
+const startScreen = document.getElementById('start-screen')
+const questionDiv = document.getElementById('questions');
+let questionTitle = document.getElementById('question-title');
+let divChoices = document.getElementById('choices');
 
 
 
@@ -13,19 +17,42 @@ setInterval(function(){
             secondsLeft--;
             timerDisplay.textContent = secondsLeft;
         if(secondsLeft === 0) {
-        alert('You have run out of time!')
-        // Stops execution of action at set interval
+        alert('You have run out of time! Try again')
+       //if times reaches 0 alert that game is lost
+       window.location.reload();//this line will refresh page if game is lost
         clearInterval(timerInterval);}
 
- },1000)}
+ },1000)};
 
-timer(6)
-
-//set up timer to count down 80secs
-//if timer ===  0 alert 'sorry you lost completely'
+timer(3)
 
 //add click event for when start button is pressed and add function that will 
 //display question and start the countdown
+function displayQuestion () {
+    startScreen.setAttribute('class','hide') //hides the start div
+    questionDiv.setAttribute('class', 'visible') //shows the questiong
+   //adds heading in form of the question
+    questionTitle.textContent = questionsObject[0].question;
+    //should set the answers into variables
+    let answerOne = document.createElement('li');
+    answerOne.innerText = questionsObject[0].answer[0]; 
+    let answerTwo = document.createElement('li');
+    answerTwo.innerText = questionsObject[0].answer[1];
+    let answerThree = document.createElement('li');
+    answerThree.innerText = questionsObject[0].answer[2];
+    let answerFour = document.createElement('li');
+    answerFour.innerText = questionsObject[0].answer[3];
+    //this should display everything
+    divChoices.append(answerOne, answerTwo, answerThree, answerFour)
+
+
+
+
+}
+
+
+
+//start.addEventListener('click', )
 
 
 //if answer is correct => next question,
--- assets/javaScript/questions.js
+++ assets/javaScript/questions.js
@@ -1,8 +1,10 @@
 //this array contains all my question stored like an object
 //together with answer options
+
 const questionsObject =[ {
     question: "blah blah popidy blah",
-    answer: ["correct", 
+    answer:  [
+            "correct", 
             "incorrect", 
             "incorrect", 
             "incorrect",], 
